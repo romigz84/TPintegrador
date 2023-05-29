0d8ea1fd902eadc492b9e563c6374db25ee5dd2c
romina <rominagonzalez84@hotmail.com>
2023-05-29 20:03:36 -0300

    arreglo en form cantidad

---
+++ ticket.js
@@ -0,0 +1,15 @@
+
+const estudiante = document.getElementsByClassName("estudiante");
+const trainee    = document.getElementsByClassName("trainee");
+const junior = document.getElementsByClassName("junior");
+const cantidad = document.getElementById("cantidad");
+
+
+
+function estudiante(cantidad) {
+if (cantidad >= 1)
+return  200*0,80*cantidad
+
+}
+
+console.log(estudiante)
