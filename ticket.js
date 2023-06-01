ac35c76a710c29f5630ecf20328f8b5d5f390bfa
romina <rominagonzalez84@hotmail.com>
2023-06-01 18:27:48 -0300

    js con formulas

--- ticket.js
+++ ticket.js
@@ -1,15 +1,27 @@
 
-const estudiante = document.getElementsByClassName("estudiante");
-const trainee    = document.getElementsByClassName("trainee");
-const junior = document.getElementsByClassName("junior");
-const cantidad = document.getElementById("cantidad");
+const estudiante = document.querySelector('#estudiante');
+const trainee    = document.querySelector('#trainee');
+const junior = document.querySelector('#junior');
+const cantidad = document.querySelector('#cantidad');
+
+const resultados = document.querySelector('#resultado');
+
+const resumen = document.querySelector('#resumen')
+
+const estudiante2 = cantidad => cantidad * 200*0.20;
+// const estudiante2 = function(cantidad){return cantidad *200*0.80}
+console.log(estudiante2(2))
+
+const trainee2 = cantidad => cantidad * 200*0.50;
+// const estudiante2 = function(cantidad){return cantidad *200*0.80}
+console.log(trainee2(2))
+
+const junior2 = cantidad => cantidad * 200*0.85;
+// const estudiante2 = function(cantidad){return cantidad *200*0.80}
+console.log(junior2(2))
 
 
 
-function estudiante(cantidad) {
-if (cantidad >= 1)
-return  200*0,80*cantidad
-
-}
-
-console.log(estudiante)
+// resumen.addEventListener('click', estudiante2){
+//     resultados.innerHTML = estudiante2.value;
+// }
