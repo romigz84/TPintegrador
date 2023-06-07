4486ae6877cbd1496c15800eac2b00279bd933da
romina <rominagonzalez84@hotmail.com>
2023-06-07 14:09:55 -0300

    calculo funcionando

--- ticket.js
+++ ticket.js
@@ -1,27 +1,35 @@
-
-const estudiante = document.querySelector('#estudiante');
-const trainee    = document.querySelector('#trainee');
-const junior = document.querySelector('#junior');
+// const estudiante = document.querySelector('#estudiante');
+// const trainee    = document.querySelector('#trainee');
+// const junior = document.querySelector('#junior');
+const resultados = document.querySelector('#resultado'); //es el P
+const resumen = document.querySelector('#resumen'); // es el boton
+resumen.addEventListener('click', precioticket);
 const cantidad = document.querySelector('#cantidad');
+// cantidad.addEventListener('click';cantidad2)
+const categoria = document.querySelector('#categoria');
+// categoria.addEventListener('click';categoria2)
 
-const resultados = document.querySelector('#resultado');
+function precioticket(evento){
+    evento.preventDefault();
+    if (Number(cantidad.value)){
+        let pagototal
+        switch(categoria.value){
+            case "estudiante": {
+                pagototal = 200*cantidad.value*0.20;
+                break;
+            }
+            case "trainee": {
+                    pagototal = 200*cantidad.value*0.50;
+                break;
+            }
+            case "junior": {
+                    pagototal = 200*cantidad.value*0.85
+                break;
+            }
+            }
+            resultados.innerHTML=pagototal
+        }
+    }
 
-const resumen = document.querySelector('#resumen')
-
-const estudiante2 = cantidad => cantidad * 200*0.20;
-// const estudiante2 = function(cantidad){return cantidad *200*0.80}
-console.log(estudiante2(2))
-
-const trainee2 = cantidad => cantidad * 200*0.50;
-// const estudiante2 = function(cantidad){return cantidad *200*0.80}
-console.log(trainee2(2))
-
-const junior2 = cantidad => cantidad * 200*0.85;
-// const estudiante2 = function(cantidad){return cantidad *200*0.80}
-console.log(junior2(2))
 
 
-
-// resumen.addEventListener('click', estudiante2){
-//     resultados.innerHTML = estudiante2.value;
-// }
